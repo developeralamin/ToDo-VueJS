653e1d040542fde0c30ae9a5f64af2f1f09d65c7
developeralamin <ribinaofficial018@gmail.com>
2022-03-25 11:27:12 +0600

    simple todo vueJs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+     <h1>VueJs todo App</h1>
+     <div class="container col-md-5">
+
+  
+      <div class="input-group mb-3">
+        <input type="text" 
+         class="form-control"
+         placeholder="Input here" 
+         v-model="task" 
+        >
+        <button  type="submit" 
+         class="btn btn-success"
+         id="button-addon2" @click="addTask">Button</button>
+      </div>
+
+   
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">SL</th>
+            <th scope="col">Task</th>
+            <th scope="col">Status</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(task,index) in tasks" :key="index">
+              <th scope="row">{{ index+1 }}</th>
+              <td>{{ task.name }}</td>
+              <td>{{ task.status }}</td>
+              <td>
+              <div @click="editTask(index)">
+              <span  class="btn btn-success btn-sm">edit</span>
+             </div>
+              <div @click="deleteTask(index)">
+              <span class="btn btn-danger btn-sm">delete</span>
+             </div>
+             
+              </td>
+          </tr>
+         </tbody> 
+    </table>
+
+
+
+     </div>
   </div>
 </template>
 
@@ -35,7 +55,63 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      task:'',
+      editedTask:null,
+      tasks:[
+         {
+          name: "Steal bananas from the supermarket.",
+          status: "to-do",
+         },
+         {
+          name: "Steal bananas from the supermarket.",
+          status: "to-do",
+         },
+      ]
+    }
+  },
+  methods: {
+
+ // deleteTask
+      deleteTask(index){
+        this.tasks.splice(index,1);
+      },
+//edit task
+
+    editTask(index){
+      this.task = this.tasks[index].name
+      this.editedTask = index
+    },
+
+    addTask(){
+      // console.log(this.task)
+      if(this.task.length === 0)return;
+
+  if(this.editedTask !== null){
+ /* We need to update the task */
+    this.tasks[this.editedTask].name = this.task
+    this.editedTask = null
+  
+ 
+    }
+
+  else{
+    /* We need to add new task */
+    this.tasks.push({
+          name: this.task,
+          status: 'todo'
+        })
   }
+        
+       this.task = null
+    },
+
+
+
+  },
+
 }
 </script>
 
